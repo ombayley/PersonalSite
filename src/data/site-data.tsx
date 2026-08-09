9e5c2569018fb50515ba8caad8b8e1f16dd8bc4d
Olly Bayley <ollybayley1@gmail.com>
2026-08-09 16:41:20 +0200

    updated email and loc

--- src/data/site-data.tsx
+++ src/data/site-data.tsx
@@ -4,8 +4,8 @@ import { SiCplusplus } from "react-icons/si";
 export const DATA = {
   name: "Dr. Olly Bayley",
   title: "Chemist • Programmer • Researcher",
-  location: "Amsterdam, NL",
-  email: "ombayley@uva.nl",
+  location: "Auckland, NZ",
+  email: "ollybayley1@gmail.com",
   links: {
     github: "https://github.com/ombayley",
     linkedin: "https://www.linkedin.com/in/ollybayleynz/",
